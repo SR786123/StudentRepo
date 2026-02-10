c6ad4430fdd0f79757989ade2523a0f97b58f436
Siyam Saman <siyamsaman7@gmail.com>
2026-02-10 05:52:29 +0000

    new change added to Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,7 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
 // Edit made by SiyamSaman_240159027.
-
+// Edit made by Siyam in Gitpod
   //Last line print('Good bye')
 }
 
